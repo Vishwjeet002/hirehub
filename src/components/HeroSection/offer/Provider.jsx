c5d0e4dfca0ed6d0d4dee10dcaf675c7bab2174c
Vishwjeet <gamerg7080@gmail.com>
2025-10-27 12:46:16 +0530

    update

--- src/components/HeroSection/offer/Provider.jsx
+++ src/components/HeroSection/offer/Provider.jsx
@@ -12,7 +12,7 @@ const Provider = () => {
   // Fetch all jobs
   const fetchJobs = async () => {
     try {
-      const response = await fetch("http://localhost:5000/jobs");
+      const response = await fetch("https://hirehub-1-3k6d.onrender.com/");
       const data = await response.json();
       setJobs(data);
     } catch (error) {
